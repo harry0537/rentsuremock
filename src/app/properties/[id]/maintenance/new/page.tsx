b63a8bad1cf5c66d45b8cb3dc88687bae0dd4f03
hazzsingh <hazsingh@icloud.com>
2025-06-13 14:07:30 +1200

    Move metadata to layout file and fix client component

--- src/app/properties/[id]/maintenance/new/page.tsx
+++ src/app/properties/[id]/maintenance/new/page.tsx
@@ -1,15 +1,7 @@
 'use client';
 
-import { Metadata } from 'next';
 import { useParams } from 'next/navigation';
-import { connectToDatabase } from '@/lib/mongodb';
-import { ObjectId } from 'mongodb';
 import MaintenanceRequestForm from '@/components/MaintenanceRequestForm';
-
-export const metadata: Metadata = {
-  title: 'New Maintenance Request | Rentsure',
-  description: 'Submit a new maintenance request for your property.',
-};
 
 export default function NewMaintenanceRequestPage() {
   const params = useParams();
